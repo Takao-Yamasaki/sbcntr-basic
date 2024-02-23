83e13eac0f0abb5abac42b8946e78d3d3660fe7b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-02-23 17:32:01 +0900

    modify:ingress

--- security_group.tf
+++ security_group.tf
@@ -1,147 +1,176 @@
-# TODO: モジュール化検討すること
-resource "aws_security_group" "ingress-1a" {
-  name = "ingress-1a"
+# インターネット公開用セキュリティグループ
+resource "aws_security_group" "sbcntr-sg-ingress" {
+  name = "sbcntr-sg-ingress"
   vpc_id = aws_vpc.sbcntrVPC.id
+  description = "security group for ingress"
+  tags = {
+    Name: "sbcntr-sg-ingress" 
+  }
+
+  # -> IGW
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    description = "allow all outbound traffic by default"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
 
-resource "aws_security_group_rule" "ingress-1a-ingress" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ingress-1a.id
-}
-
-resource "aws_security_group_rule" "ingress-1a-engress" {
-  type = "engress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.ingress-1a.id
-}
-
-resource "aws_security_group" "frontend-app-1a" {
-  name = "frontend-app-1a"
+# 管理用サーバー向けのセキュリティグループ
+resource "aws_security_group" "sbcntr-sg-management" {
+  name = "sbcntr-sg-management"
   vpc_id = aws_vpc.sbcntrVPC.id
+  description = "security group for management server"
+  
+  tags = {
+    Name: "sbcntr-sg-management"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    description = "allow all outbound traffic by default"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
 
-resource "aws_security_group_rule" "frontend-app-1a-ingress" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.frontend-app-1a.id
-}
-
-resource "aws_security_group_rule" "frontend-app-1a-engress" {
-  type = "engress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.frontend-app-1a.id
-}
-
-resource "aws_security_group" "internal-alb-1a" {
-  name = "internal-alb-1a"
+# バックエンドコンテナアプリ用のセキュリティグループ
+resource "aws_security_group" "sbcntr-sg-container" {
+  name = "sbcntr-sg-container"
   vpc_id = aws_vpc.sbcntrVPC.id
+  description = "security group of backend app"
+
+  tags = {
+    Name: "sbcntr-sg-container"
+  }
+
+# Internal LB -> Backend Container
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    description = "HTTP for internal lb"
+    security_groups = [ aws_security_group.sbcntr-sg-internal.id ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    description = "allow all outbound traffic by default"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
 
-resource "aws_security_group_rule" "internal-alb-1a-ingress" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.internal-alb-1a.id
-}
-
-resource "aws_security_group_rule" "internal-alb-1a-engress" {
-  type = "engress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.internal-alb-1a.id
-}
-
-resource "aws_security_group" "backend-app-1a" {
-  name = "backend-app-1a"
+# フロントエンドコンテナアプリ用のセキュリティグループ
+resource "aws_security_group" "sbcntr-sg-front-container" {
+  name = "sbcntr-sg-front-container"
   vpc_id = aws_vpc.sbcntrVPC.id
+  description = "security group of front container app"
+
+  tags = {
+    Name: "sbcntr-sg-front-container"
+  }
+
+  # Internet LB -> Frontend Container
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    description =  "HTTP for Ingress"
+    security_groups = [ aws_security_group.sbcntr-sg-ingress.id ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    description = "allow all outbound traffic by default"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
 
-resource "aws_security_group_rule" "backend-app-1a-ingress" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.backend-app-1a.id
-}
-
-resource "aws_security_group_rule" "backend-app-1a-engress" {
-  type = "engress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.backend-app-1a.id
-}
-
-resource "aws_security_group" "db-1a" {
-  name = "db-1a"
+# 内部用ロードバランサー用のセキュリティグループ
+resource "aws_security_group" "sbcntr-sg-internal" {
+  name = "sbcntr-sg-internal"
   vpc_id = aws_vpc.sbcntrVPC.id
+  description = "security group for internal load balancer"
+  tags = {
+    Name: "sbcntr-sg-internal"
+  }
+
+  # Frontend Container -> Internal LB
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    description = "HTTP for frontend container"
+    security_groups = [ aws_security_group.sbcntr-sg-front-container.id ]
+  }
+  # Management Container -> Internal LB 
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    description = "HTTP for management server"
+    security_groups = [ aws_security_group.sbcntr-sg-management.id ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    description = "allow all outbound traffic by default"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
 
-resource "aws_security_group_rule" "db-1a-ingress" {
-  type = "ingress"
-  from_port = "3306"
-  to_port = "3306"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.db-1a.id
-}
-
-resource "aws_security_group_rule" "db-1a-engress" {
-  type = "engress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.db-1a.id
-}
-
-resource "aws_security_group" "management-1a" {
-  name = "management-1a"
+# DB用セキュリティグループ
+resource "aws_security_group" "sbcntr-sg-db" {
+  name = "sbcntr-sg-db"
   vpc_id = aws_vpc.sbcntrVPC.id
-}
+  description = "security group of database"
 
-resource "aws_security_group_rule" "management-1a-http-ingress" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.management-1a.id
-}
+  tags = {
+    Name: "sbcntr-sg-internal"
+  }
 
-resource "aws_security_group_rule" "management-1a-db-ingress" {
-  type = "ingress"
-  from_port = "3306"
-  to_port = "3306"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.management-1a.id
-}
-
-resource "aws_security_group_rule" "management-1a-engress" {
-  type = "engress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.management-1a.id
+  # Backend container -> DB
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    description = "MySQL protocol from backend app"
+    security_groups = [ aws_security_group.sbcntr-sg-container.id ]
+  }
+  # Frontend container -> DB
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    description = "MySQL protocol from frontend app"
+    security_groups = [ aws_security_group.sbcntr-sg-front-container.id ]
+  }
+  # Management container -> DB
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    description = "MySQL protocol from management server"
+    security_groups = [ aws_security_group.sbcntr-sg-management.id ]
+  }
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    description = "allow all outbound traffic by default"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
