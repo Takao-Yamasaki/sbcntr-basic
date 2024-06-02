ad94ae394ca7585a23c9af257f7f62047ac56ea4
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-06-03 07:40:06 +0900

    add:lificycle

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,30 +1,111 @@
-# # NOTE: 作業中のためコメントアウト
-# # プライベートリポジトリ バックエンド
-# resource "aws_ecr_repository" "sbcntr-backend" {
-#   name                 = "sbcntr-backend"
-#   image_tag_mutability = "MUTABLE"
+# terraform import
+## https://tech.layerx.co.jp/entry/improve-iac-development-with-terraform-import
 
-#   image_scanning_configuration {
-#     scan_on_push = false
-#   }
-# }
+# プライベートリポジトリ バックエンド
+## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository.html
+resource "aws_ecr_repository" "sbcntr-backend" {
+  name                 = "sbcntr-backend"
 
-# # プライベートリポジトリ フロントエンド
-# resource "aws_ecr_repository" "sbcntr-frontend" {
-#   name                 = "sbcntr-frontend"
-#   image_tag_mutability = "MUTABLE"
+  # タグの重複を許可しない
+  image_tag_mutability = "IMMUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = false
-#   }
-# }
+  # 脆弱性スキャンの有効化
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# ライフサイクルポリシー バックエンド
+## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_lifecycle_policy
+resource "aws_ecr_lifecycle_policy" "sbcntr-backend" {
+  repository = aws_ecr_repository.sbcntr-backend.name
+  policy = <<-EOT
+  {
+    "roles": [
+      {
+        "rolePriority": 1,
+        "description": "古い世代のイメージを削除",
+        "selection": {
+          "countUint": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOT
+}
+
+# プライベートリポジトリ フロントエンド
+resource "aws_ecr_repository" "sbcntr-frontend" {
+  name                 = "sbcntr-frontend"
+
+  # タグの重複を許可しない
+  image_tag_mutability = "IMMUTABLE"
+
+  # 脆弱性スキャンの有効化
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# ライフサイクルポリシー フロントエンド
+## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_lifecycle_policy
+resource "aws_ecr_lifecycle_policy" "sbcntr-frontend" {
+  repository = aws_ecr_repository.sbcntr-frontend.name
+  policy = <<-EOT
+  {
+    "roles": [
+      {
+        "rolePriority": 1,
+        "description": "古い世代のイメージを削除",
+        "selection": {
+          "countUint": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOT
+}
 
 # プライベートリポジトリ(共通のベースイメージ)
-# resource "aws_ecr_repository" "sbcntr-base" {
-#   name                 = "sbcntr-base"
-#   image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "sbcntr-base" {
+  name                 = "sbcntr-base"
+  
+  # タグの重複を許可しない
+  image_tag_mutability = "IMMUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = false
-#   }
-# }
+  # 脆弱性スキャンの有効化
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# ライフサイクルポリシー 共通のベースイメージ
+## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_lifecycle_policy
+resource "aws_ecr_lifecycle_policy" "sbcntr-base" {
+  repository = aws_ecr_repository.sbcntr-base.name
+  policy = <<-EOT
+  {
+    "roles": [
+      {
+        "rolePriority": 1,
+        "description": "古い世代のイメージを削除",
+        "selection": {
+          "countUint": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOT
+}
